5cef44d7e021f8d4cee2df4a80b3a9d947a5e3a7
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2020-05-19 02:26:46 -0300

    Classe Post adicionada

---
+++ Project/Entidy/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Entidy
+{
+    class Post
+    {
+        public string Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+
+        public List<Comment> comments { get; set; } = new List<Comment>();
+        public Post()
+        {
+
+        }
+
+        public Post(string moment, string title, string content, int like)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Like = like;
+        }
+    }
+}
